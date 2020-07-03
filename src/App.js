76d907eb4592f0fc03b37186f4666ceff15a465d
Vinent Bouquet <bouquet.vincent704@orange.fr>
2020-07-03 09:14:59 +0200

    exo-react-pendu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import letters from './letters.json'
+import words from './words.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    const [word, setWord] = useState(editRandomWord())
+    const [letter, setLetter] = useState("_")
+
+    function editLetter(event) {
+        setLetter (event.target.value)
+    }
+
+    function editWord() {
+        setWord (editRandomWord())
+    }
+
+    function editRandomWord() {
+        //setWord ( words[ Math.floor(Math.random()*(words.length) ) ] )
+        return words[ Math.floor(Math.random()*(words.length) ) ]
+    }
+
+    function displayButtonLetters() {
+        return letters.map( (letter) => <button value={letter} onClick={editLetter}>{letter.toUpperCase()}</button> )
+    }
+
+
+    return (
+        <div className="App">
+            <button onClick={editWord}>Choisir un nouveau mot</button>
+            <div>
+                {word}
+            </div>
+            <div>
+                {displayButtonLetters()}
+            </div>
+        </div>
+    )
+}
 export default App;
